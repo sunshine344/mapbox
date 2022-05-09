f24e9f6719661071e299848305eefc778fd65f38
gouqingping <gouqingping@yahoo.com>
2022-05-09 11:11:15 +0800

    add common

--- project/src/config/amb.ts
+++ project/src/config/amb.ts
@@ -1,14 +1,11 @@
-
-        
-export const ENV = "dev";
-export const ENV_URL = "ambiences.config.json";
+export const ENV = 'dev';
+export const ENV_URL = 'ambiences.config.json';
 export const api = {
-     "BASE_URL":"",
+	BASE_URL: '',
 };
 export const config = {
-     "mock":"",
-     "systemName":"DevDependenciessystemname",
+	mock: '',
+	systemName: 'DevDependenciessystemname',
 };
-        
-export default {ENV, ENV_URL,api,config}
-    
+
+export default { ENV, ENV_URL, api, config };
