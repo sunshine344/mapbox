e6846acc31e1dca85a8639468d197e6a3b9c61ae
gouqingping <gouqingping@yahoo.com>
2022-09-20 16:20:48 +0800

    update template

--- project/src/config/amb.ts
+++ project/src/config/amb.ts
@@ -1,15 +1,15 @@
+interface AnyObject {
+	[key: string]: any;
+}
 
-        interface AnyObject { [key: string]: any }
+export const ENV = 'dev';
+export const ENV_URL = 'ambiences.config.json';
+export const api: AnyObject = {
+	BASE_URL: '',
+};
+export const config: AnyObject = {
+	mock: true,
+	systemName: '前端开发模版-template-vue',
+};
 
-export const ENV:string = "dev";
-export const ENV_URL:string = "ambiences.config.json";
-export const api:AnyObject = {
-     "BASE_URL":"",
-};
-export const config:AnyObject = {
-     "mock":true,
-     "systemName":"前端开发模版-template-vue",
-};
-        
-export default {ENV, ENV_URL,api,config}
-    
+export default { ENV, ENV_URL, api, config };
